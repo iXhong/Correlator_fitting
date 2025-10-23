fix:correct flip & average part in data_load

bootstrap_fit.py
@@ -1,35 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from lmfit import minimize, Parameters, fit_report
-import glob
-
-
-def data_load():
-    """ 
-    load correlator data from .dat files
-    Return:
-        flip_ fliped & averaged data
-        t: time data
-        N: number of correlator files
-    """
-   
-    file_list = sorted(glob.glob("./mass/*.dat"))
-
-    real_data_all = []
-
-    for fname in file_list:
-        data = np.loadtxt(fname, comments="#")
-        filtered = data[data[:, 3] == 0]  # 第4列是 mumu
-        real_values = filtered[:, 5]
-        real_data_all.append(real_values)
-    C_array = np.array(real_data_all)
-
-    N = C_array.shape[0]  # files num
-    flip_data = (C_array[:, :48] + np.flip(C_array[:, -48:])) / 2  # flip & average data
-    t = np.arange(flip_data.shape[1])
-    print(f"{N} 组数据，{len(t)} 个时间点")
-
-    return flip_data, t, N
+from load_data import data_load
 
 
 def fit_function_cosh(params, t, T):
@@ -45,6 +17,7 @@ def residual(params, t_fit, data_fit, err_fit, T):
     Return:
     """
     model = fit_function_cosh(params, t_fit, T)
+    # np.sum()
     return (data_fit - model) / err_fit
 
 
@@ -67,22 +40,24 @@ def bootstrap_fit(t_min: int,t_max: int,t,data,T: int,n_resamples: int = 500,pri
     """
 
     rng = np.random.default_rng(random_seed)
-    N_cfg = data.shape[0]
+    N_block = data.shape[0]
     fit_mask = (t >= t_min) & (t <= t_max)
     t_fit = t[fit_mask]
 
     # 构造 bootstrap samples (对配置取平均)
     bs_means = []
     for _ in range(n_resamples):
-        chosen = rng.integers(0, N_cfg, N_cfg)  # 有放回重抽样
+        chosen = rng.integers(0, N_block, N_block)  # 有放回重抽样
         bs_subdata = data[chosen][:, fit_mask]  #bs resampled 
         bs_mean = np.mean(bs_subdata, axis=0)
         bs_means.append(bs_mean)
     bs_means = np.array(bs_means)  # shape (n_resamples, n_tfit) aka bootstrap samples
 
     # 全局误差（每个时间点的标准差）
-    sigma_global = np.std(bs_means, axis=0, ddof=1)
-
+    sigma_global = np.sqrt(N_block/(N_block-1))*np.std(bs_means, axis=0, ddof=1)
+    print(sigma_global)
+    # sigma_global = np.std(bs_means,axis=0,ddof=1)
+    # return 0
     # 对每个 bootstrap sample 拟合
     bs_params_A0 = []
     bs_params_m0 = []
@@ -198,7 +173,7 @@ def main(tmin, tmax, show_plot, print_report, n_resamples=50):
 
 
 if __name__ == "__main__":
-    out = main(tmin=5, tmax=35, show_plot=False, print_report=True, n_resamples=10)
+    out = main(tmin=5, tmax=35, show_plot=False, print_report=True, n_resamples=100)
     
 
     # print(out.redchi)

direct_fit.py
@@ -2,26 +2,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 from lmfit import minimize, Parameters, fit_report
 from latqcdtools.statistics.jackknife import jackknife
-import glob
+from load_data import data_load
 
-def data_load():    
-    file_list = sorted(glob.glob("./mass/*.dat"))
-
-    real_data_all = []
-
-    for fname in file_list:
-        data = np.loadtxt(fname, comments='#')
-        filtered = data[data[:, 3] == 0]  # 第4列是 mumu
-        real_values = filtered[:, 5]
-        real_data_all.append(real_values)
-    C_array = np.array(real_data_all)
-
-    N_cfg = C_array.shape[0] # configuration num
-    flip_data = (C_array[:,:48] + np.flip(C_array[:,-48:]))/2 #flip & average data
-    t = np.arange(flip_data.shape[1])
-    print(f'{N_cfg} 个组态，{len(t)} 个时间点')
-
-    return flip_data, t, N_cfg
 
 def fit_function(params, t):
     """使用单指数函数"""
@@ -59,8 +41,8 @@ def direct_fit(t_min, t_max, t, data,T,print_report):
     """改进的拟合函数，考虑统计误差"""
     t_fit, data_fit, err_fit = jk_mean_err(t_min, t_max, t, data)
     
-    print(f"拟合数据范围: {data_fit.min():.2e} 到 {data_fit.max():.2e}")
-    print(f"典型误差: {np.mean(err_fit):.2e}")
+    # print(f"拟合数据范围: {data_fit.min():.2e} 到 {data_fit.max():.2e}")
+    # print(f"典型误差: {np.mean(err_fit):.2e}")
     
     params = Parameters()
     params.add('A0', value=data_fit[0], min=0)  
@@ -134,7 +116,7 @@ def main(show_plot,print_report):
     mean_corr = np.mean(data, axis=0)
     T = 96
 
-    result, t_fit, data_fit, err_fit = direct_fit(t_min=5, t_max=16, t=t, data=data,T=T,print_report=print_report)
+    result, t_fit, data_fit, err_fit = direct_fit(t_min=5, t_max=48, t=t, data=data,T=T,print_report=print_report)
     
     if show_plot:
         # plot_fit_result(t, mean_corr, result, t_fit, data_fit)
@@ -152,3 +134,5 @@ def main(show_plot,print_report):
 if __name__ == "__main__":
 
     out = main(show_plot=False,print_report=True)
+    # filp_data,t,N = data_load()
+    # print(filp_data.shape)

jackknife_fit.py
@@ -1,36 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from lmfit import minimize, Parameters, fit_report
-import glob
-
-
-def data_load():
-    """ 
-    load correlator data from .dat files
-    Return:
-        flip_data: fliped & averaged data
-        t: time data
-        N: number of correlator files
-    """
-   
-    file_list = sorted(glob.glob("./mass/*.dat"))
-
-    real_data_all = []
-
-    for fname in file_list:
-        data = np.loadtxt(fname, comments="#")
-        filtered = data[data[:, 3] == 0]  # 第4列是 mumu
-        real_values = filtered[:, 5]
-        real_data_all.append(real_values)
-    C_array = np.array(real_data_all)
-
-    N = C_array.shape[0]  # files num
-    flip_data = (C_array[:, :48] + np.flip(C_array[:, -48:])) / 2  # flip & average data
-    t = np.arange(flip_data.shape[1])
-    print(f"{N} 组数据，{len(t)} 个时间点")
-
-    return flip_data, t,N
-
+from load_data import data_load
 
 def fit_function_cosh(params, t, T):
     A0 = params["A0"]
@@ -79,6 +50,9 @@ def jackknife_fit(t_min: int, t_max: int, t, data, T: int, print_report: bool = 
     diffs = jk_means - jk_mean_overall
     # jackknife 方差公式 (N-1)/N * sum (diff^2)
     sigma_global = np.sqrt((N_blocks - 1) / N_blocks * np.sum(diffs**2, axis=0))
+
+    print(sigma_global)
+    # return 0
     # 这里如果要做相关拟合，也可以在这一步构造协方差矩阵 C
 
     # 对每个 jackknife sample 做拟合
@@ -194,7 +168,7 @@ def main(tmin, tmax, show_plot, print_report):
 
 
 if __name__ == "__main__":
-    out = main(tmin=5, tmax=48, show_plot=False, print_report=True)
+    out = main(tmin=5, tmax=35, show_plot=False, print_report=True)
 
     # print(out.redchi)
     print(out.aicc)

tmin_scan.py
@@ -125,7 +125,7 @@ def plot(type):
     Plots data from 'jk_tmin_scan.npz' on two subplots of a single figure.
     One subplot has a free y-axis scale, the other has a fixed y-axis scale (-25, 30).
     """
-    data = np.load("bs_tmin_scan.npz")
+    data = np.load("jk_tmin_scan.npz")
     tmin_list, y = data['tmin'], data[f'{type}']
 
     # 创建一个包含两个子图的图形，布局为 2 行 1 列
@@ -157,7 +157,7 @@ def plot(type):
 
 def mass_dual_y():
 
-    data2 = np.load("bs_tmin_scan.npz")
+    data2 = np.load("jk_tmin_scan.npz")
     t_list2, m0_2, m0_err_2,redchi2,aicc2 = data2['tmin'], data2['m0'], data2['err'],data2['redchi'],data2['aicc']
     
     fig,ax1 = plt.subplots()
@@ -165,10 +165,10 @@ def mass_dual_y():
                 capsize=3, alpha=0.5, label=r'$am_0$')
     ax1.set_ylabel(r"$am_0$")
     ax2 = ax1.twinx()
-    lines2 = ax2.scatter(t_list2,redchi2,label="redchi2")
-    ax2.set_ylabel("redchi2")
-    ax2.hlines(1,xmin=1,xmax=24,linestyles='dashed',label="redchi2=1")
-    # ax2.set_ylim(-5,5)
+    lines2 = ax2.scatter(t_list2,aicc2,label="aicc2")
+    ax2.set_ylabel("aicc2")
+    ax2.hlines(1,xmin=1,xmax=24,linestyles='dashed',label="aicc2=0")
+    ax2.set_ylim(-50,100)
 
     lines1, labels1 = ax1.get_legend_handles_labels()
     lines2, labels2 = ax2.get_legend_handles_labels()
@@ -180,8 +180,8 @@ def mass_dual_y():
 
 if __name__ == "__main__":
 
-    # mass_dual_y()
-    plot("aicc")
+    mass_dual_y()
+    # plot("aicc")
 
     # tmin_list,m0_d,m0_err_d = direct_fit_tmin_scan(tmin_left=1,tmin_right=24,tmax=48,T=96)
     # tmin_list ,m0_j,m0_err_j,redchi_j,aicc_j = jackknife_fit_tmin_scan(tmin_left=1,tmin_right=24,tmax=40,T=96)

two_cosh_fit.py
@@ -2,26 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 from lmfit import minimize, Parameters, fit_report
 from latqcdtools.statistics.jackknife import jackknife
-import glob
-
-def data_load():    
-    file_list = sorted(glob.glob("./mass/*.dat"))
-
-    real_data_all = []
-
-    for fname in file_list:
-        data = np.loadtxt(fname, comments='#')
-        filtered = data[data[:, 3] == 0]  # 第4列是 mumu
-        real_values = filtered[:, 5]
-        real_data_all.append(real_values)
-    C_array = np.array(real_data_all)
-
-    N_cfg = C_array.shape[0] # configuration num
-    flip_data = (C_array[:,:48] + np.flip(C_array[:,-48:]))/2 #flip & average data
-    t = np.arange(flip_data.shape[1])
-    print(f'{N_cfg} 个组态，{len(t)} 个时间点')
-
-    return flip_data, t, N_cfg
+from load_data import data_load
 
 
 def func(data):

two_state_fit_cosh.py
@@ -2,25 +2,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 from latqcdtools.statistics.jackknife import jackknife
 from lmfit import minimize,Parameters, fit_report
-import glob
+from load_data import data_load
 
-def data_load():    
-    file_list = sorted(glob.glob("./mass/*.dat"))
-    real_data_all = []
-
-    for fname in file_list:
-        data = np.loadtxt(fname, comments='#')
-        filtered = data[data[:, 3] == 0]
-        real_values = filtered[:, 5]
-        real_data_all.append(real_values)
-    C_array = np.array(real_data_all)
-
-    N_cfg = C_array.shape[0]
-    flip_data = (C_array[:,:48] + np.flip(C_array[:,-48:]))/2
-    t = np.arange(flip_data.shape[1])
-    print(f'{N_cfg} 个组态，{len(t)} 个时间点')
-
-    return flip_data, t, N_cfg
 
 def func(data):
     return np.mean(data,axis=0)
@@ -62,21 +45,36 @@ def two_state_fit(t_fit,data_fit,err_fit,T,scale_factor):
 
     return result
 
+
 if __name__ == "__main__":
     data, t, N_cfg = data_load()
     T = 96
 
-    t_fit, data_fit, err_fit, scale_factor = jackknife_fit(t_min=5, t_max=16, t=t, data=data)  # ⚠️ 完全按照程序2的调用方式
+    t_fit, data_fit, err_fit, scale_factor = jackknife_fit(t_min=5, t_max=16, t=t, data=data) 
 
-    out = two_state_fit(t_fit=t_fit, data_fit=data_fit, err_fit=err_fit, T=T, scale_factor=scale_factor)
-    print(fit_report(out))
+    # out = two_state_fit(t_fit=t_fit, data_fit=data_fit, err_fit=err_fit, T=T, scale_factor=scale_factor)
+    # print(fit_report(out))
 
-    print("="*50)
-    print("真实的拟合结果如下")
-    print(f"A0= {out.params['A0'].value / scale_factor:.6e}")
-    print(f"A1= {out.params['A1'].value / scale_factor:.6e}")
-    print(f"m0= {out.params['m0'].value:.6f}")
-    print(f"m1= {out.params['m1'].value:.6f}")
+    # print("="*50)
+    # print("真实的拟合结果如下")
+    # print(f"A0= {out.params['A0'].value / scale_factor:.6e}")
+    # print(f"A1= {out.params['A1'].value / scale_factor:.6e}")
+    # print(f"m0= {out.params['m0'].value:.6f}")
+    # print(f"m1= {out.params['m1'].value:.6f}")
+
+        # 新增的稳定性分析
+    t_min_list, m0_values, m0_errors = analyze_m0_stability(
+        data=data, t=t, N_cfg=N_cfg, T=T, 
+        t_max=16, t_min_range=(3, 12)
+    )
+
+    # plt.figure()
+    # plt.errorbar(t_min_list,m0_values,m0_errors,fmt='o')
+    # plt.ylim((0.6,0.65))
+    # plt.show()
+
+    # 绘制稳定性图
+    plot_m0_stability(t_min_list, m0_values, m0_errors, t_max=16)
 
 
     