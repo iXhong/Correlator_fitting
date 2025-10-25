ff3e914a55f508e0808471bf023731f9fc8f2ec7
George Liu <125779780+iXhong@users.noreply.github.com>
2025-10-25 17:22:10 +0800

    correct some variable name

--- two_cosh_fit.py
+++ two_cosh_fit.py
@@ -10,14 +10,15 @@ def func(data):
 
 
 def jk_mean_err(t_min,t_max,t,data):
-    N_cfg = data.shape[0]
+    #get data to be fitted with jackknife
+    N_blocks = data.shape[0]
     fit_mask = (t >= t_min) & (t <= t_max)
     t_fit = t[fit_mask]
 
     #scale the data
     scale_factor = 1e10
     data = data * scale_factor
-    jk_mean, jk_err = jackknife(f=func,data=data,numb_blocks=N_cfg,conf_axis=0,return_sample=False)
+    jk_mean, jk_err = jackknife(f=func,data=data,numb_blocks=N_blocks,conf_axis=0,return_sample=False)
 
     data_fit = jk_mean[fit_mask]
     err_fit = jk_err[fit_mask]
@@ -26,6 +27,7 @@ def jk_mean_err(t_min,t_max,t,data):
 
 
 def two_cosh_func(params,t,T):
+    #function to fit
     A0 = params['A0']
     A1 = params['A1'] 
     m0 = params['m0']
@@ -34,28 +36,27 @@ def two_cosh_func(params,t,T):
 
 
 def double_cosh_residual(params, t_fit, data_fit, err_fit,T):
-    """双指数残差函数"""
+    #双指数残差函数
     model = two_cosh_func(params, t_fit,T)
     return (data_fit - model) / err_fit
 
 
 def two_state_fit(t_fit,data_fit,err_fit,T,scale_factor):
-    # 2. 双指数拟合
+    #双指数拟合
     params_double = Parameters()
-    # 初始参数设置很重要
     params_double.add('A0', value=1e-15*scale_factor, min=1e-16*scale_factor)
     params_double.add('A1', value=1e-17*scale_factor, min=0, max=1e-7*scale_factor)
-    # params_double.add('m0', value=0.60073944, min=0.6, max=1.5)        # 基态
-    # params_double.add('m1', value=1.0, min=0.7, max=3.0)        # 激发态，应该更大
-    params_double.add('m0', value=0.60, min=0.6, max=0.8)  # ⚠️ 更严格的上界
+    params_double.add('m0', value=0.60, min=0.6, max=0.8)
     params_double.add('m1', value=1.0, min=0.7, max=5.0)
     
-    result_double = minimize(double_cosh_residual, params_double, method='least_squares',
+    result = minimize(double_cosh_residual, params_double, method='least_squares',
                            kws={"t_fit": t_fit, "data_fit": data_fit, "err_fit": err_fit,"T":T})
 
-    return result_double
+    return result
 
-def out_plot(out, data, scale_factor, t_fit):
+
+def out_plot(out, data, scale_factor, t_fit, t, T):
+    #plot 
     A0 = out.params['A0'].value / scale_factor
     A1 = out.params['A1'].value / scale_factor
     m0 = out.params['m0'].value
@@ -79,16 +80,18 @@ def out_plot(out, data, scale_factor, t_fit):
     plt.show()
 
 
-if __name__ == "__main__":
-    data, t, N_cfg = data_load()
+def main(show_plot=True,show_report=True):
+    data, t,_= data_load()
     T = 96
 
     t_fit, data_fit, err_fit, scale_factor = jk_mean_err(t_min=5, t_max=16, t=t, data=data)
 
     out = two_state_fit(t_fit=t_fit, data_fit=data_fit, err_fit=err_fit, T=T, scale_factor=scale_factor)
-    print(fit_report(out))
+    if show_report:
+        print(fit_report(out))
 
-    out_plot(out, data, scale_factor, t_fit)  # 传入t_fit
+    if show_plot:
+        out_plot(out, data, scale_factor, t_fit, t, T)  # 传入t_fit
 
     print("="*50)
     print("真实的拟合结果如下")
@@ -97,8 +100,13 @@ if __name__ == "__main__":
     print(f"m0= {out.params['m0'].value:.6f}")
     print(f"m1= {out.params['m1'].value:.6f}")
 
+
+if __name__ == "__main__":
+
+    main()
+
 # if __name__ == "__main__":
-#     data, t, N_cfg = data_load()
+#     data, t, N_blocks = data_load()
 #     T = 96
 #     t_min, t_max = 5, 16
     
